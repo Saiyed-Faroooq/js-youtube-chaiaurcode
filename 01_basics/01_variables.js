62cfde30e6d1f45d03fd9521655e10140b375ceb
Saiyed-Faroooq <147801152+Saiyed-Faroooq@users.noreply.github.com>
2025-08-06 09:49:24 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let username = "Raj";
+var nameID = "123466";
+usercity = "Surat";
+let userstate;
+
+// Changing the value of a variable prefixed by const is not allowed, once set.
+
+username = "Shyam";
+nameID = "123555";
+usercity = "Rajkot";
+
+/* now to print the output in terminal use consol.log(" "). But to print all the variable at a time use console.table([]) and write the variables seperating it with a comma.*/
